253bee2d63b728d7710a68a1055bee4b6bf1016b
pranav.n-at-411977029542 <pranav.n-at-411977029542>
2023-01-07 14:42:02 +0530

    Added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import ProjectsPage from './components/projectsPage';
+
+
+function App() {
+  
+  return (
+   <div className='container'>
+    <ProjectsPage />
+   </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/projectsPage.tsx
@@ -0,0 +1,4 @@
+function ProjectsPage() {
+  return <h1>Tasks Logs</h1>
+}
+export default ProjectsPage
