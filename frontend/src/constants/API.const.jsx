48bf21c74c7fe6cca6aab48900d1f132e62caf4a
1001Pranav <npranavr@gmail.com>
2023-03-20 12:17:57 +0530

    Added constant

---
+++ frontend/src/constants/API.const.jsx
@@ -0,0 +1,11 @@
+export const API_ENDPOINT = "http://localhost:3002/";
+
+export const API_RESOURCE = {
+  "login": "user/login",
+  "register": "user/register",
+  "taskList": "task/list",
+}
+
+export const RESPONSE_STATUS = {
+  "SUCCESS": 200,
+}
